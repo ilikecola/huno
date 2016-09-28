4859e13d746fef1f1ca7ce84815de9877ee86975
ilikecola <knight.lost.heaven@gmail.com>
2016-09-28 23:24:27 +0800

    modified image

--- source/js/main.js
+++ source/js/main.js
@@ -78,8 +78,6 @@ $(document).ready(function() {
   
 	if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed');
-    $("img.excerpt").css('height','50%');
-    $("img.excerpt").css('width','50%');
   }
   
   if(!($('.panel-cover').hasClass('panel-cover--collapsed'))){
